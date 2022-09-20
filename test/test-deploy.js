e44154c35a298148f4508f764285f26e1081eb9d
Gennadion <ghammurti@gmail.com>
2022-09-20 19:22:10 +0300

    Initial Commit

---
+++ test/test-deploy.js
@@ -0,0 +1,38 @@
+const { ethers } = require("hardhat");
+const { expect, assert } = require("chai");
+
+describe("SimpleStorage", () => {
+	let simpleStorageFactory, simpleStorage;
+	beforeEach(async function () {
+		simpleStorageFactory = await ethers.getContractFactory("SimpleStorage");
+		simpleStorage = await simpleStorageFactory.deploy();
+	});
+
+	it("Should start with a favourite number of 0", async () => {
+		const currentValue = await simpleStorage.retrieve();
+		const expectedValue = "0";
+
+		assert.equal(currentValue.toString(), expectedValue);
+	});
+
+	it("Should update when we call store", async () => {
+		const expectedValue = "7";
+		const transactionResponse = await simpleStorage.store(expectedValue);
+		await transactionResponse.wait(1);
+
+		const currentValue = await simpleStorage.retrieve();
+		assert.equal(currentValue.toString(), expectedValue);
+    //expect(currentValue.toString()).to.equal(expectedValue)
+	});
+
+	it("Should update when we call addPerson", async () => {
+		const expectedName = "John";
+		const expectedValue = "7";
+		const transactionResponse = await simpleStorage.addPerson(expectedName, expectedValue);
+		await transactionResponse.wait(1);
+
+		const currentValue = await simpleStorage.nameToFavoriteNumber(expectedName);
+		assert.equal(currentValue.toString(), expectedValue);
+    //expect(currentValue.toString()).to.equal(expectedValue)
+	});
+});
